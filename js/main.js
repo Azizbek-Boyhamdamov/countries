d7d87404cf01dd0761ceedea11a295db9709880d
Azizbek-Boyhamdamov <boyhamdamovazizbek27@gmail.com>
2022-04-12 17:41:25 +0500

    Add link to details-page

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const elDarkModeToggle = document.querySelector(".dark-mode-toggle");
-
-elDarkModeToggle.addEventListener('click', function(){
-  document.body.classList.toggle('dark-mode');
-});
-
